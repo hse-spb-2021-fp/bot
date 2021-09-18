b7484074753e8088db5ad421458c1b2af15ce41d
Vasily Alferov <vasily.v.alferov@gmail.com>
2021-09-18 13:45:08 +0300

    z

--- lib/bot.ml
+++ lib/bot.ml
@@ -8,7 +8,7 @@ let process_hook body _req =
   (match Event.of_string body with
   | Ok (Pull_request pr) ->
     Log.Global.info_s [%message "Pull request" (pr : Event.Pull_request.t)]
-  | Error _ -> ());
+  | Error e -> Log.Global.error_s [%message "Error" (e : Error.t)]);
   Server.respond_string "<h1>Hello from fp.vasalf.net</h1>"
 ;;
 
