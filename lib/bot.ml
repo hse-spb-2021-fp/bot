1588654eb252735adabf0e3ba7c33c991ead7af1
Vasily Alferov <vasily.v.alferov@gmail.com>
2021-09-18 12:04:34 +0300

    Version 0.1

---
+++ lib/bot.ml
@@ -0,0 +1,22 @@
+open Core
+open Async
+open Cohttp
+open Cohttp_async
+
+let server =
+  let%map_open.Command () = return () in
+  fun () ->
+    let callback ~body addr req =
+      let uri = Uri.to_string (Request.uri req) in
+      let meth = Request.meth req in
+      Log.Global.info_s
+        [%message (addr : Socket.Address.Inet.t) uri (meth : Code.meth) (body : Body.t)];
+      Server.respond_string "<h1>Hello from fp.vasalf.net</h1>"
+    in
+    let where_to_listen = Tcp.Where_to_listen.of_port 8179 in
+    let%bind server = Server.create ~on_handler_error:`Raise where_to_listen callback in
+    ignore server;
+    Deferred.never ()
+;;
+
+let command = Command.async ~summary:"Run the bot" server
