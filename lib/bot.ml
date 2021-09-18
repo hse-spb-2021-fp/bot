6ef13549a535c342d2cf76516880e8549b169790
Vasily Alferov <vasily.v.alferov@gmail.com>
2021-09-18 18:20:05 +0300

    z

--- lib/bot.ml
+++ lib/bot.ml
@@ -5,15 +5,38 @@ open Cohttp_async
 open Types
 
 module Github_creds = struct
-  type t =
-    { client_id : string
-    ; client_secret : string
-    }
-  [@@deriving sexp]
+  type t = { iss : int } [@@deriving sexp]
 
-  let to_auth_string { client_id; client_secret } =
-    let b64creds = Base64.encode_exn [%string "%{client_id}:%{client_secret}"] in
-    [%string "Basic %{b64creds}"]
+  let to_auth_string { iss } =
+    let%bind key =
+      Reader.with_file "hse-spb-2021-fp.2021-09-18.private-key.pem" ~f:(fun reader ->
+          let%bind s = Reader.contents reader in
+          let cst = Cstruct.of_string s in
+          Deferred.return (Option.value_exn (Result.ok (X509.Private_key.decode_pem cst))))
+    in
+    let key =
+      match key with
+      | `RSA key -> key
+      | _ -> raise_s [%message "unknown key type"]
+    in
+    let jwk = Jose.Jwk.make_priv_rsa key in
+    let header = Jose.Header.make_header jwk in
+    let iat =
+      Int.of_float (Time.Span.to_sec (Time.to_span_since_epoch (Time.now ()))) - 60
+    in
+    let exp = iat + 600 in
+    let payload =
+      Jose.Jwt.empty_payload
+      |> Jose.Jwt.add_claim "iat" (`Int iat)
+      |> Jose.Jwt.add_claim "exp" (`Int exp)
+      |> Jose.Jwt.add_claim "iss" (`Int iss)
+    in
+    let jwt =
+      match Jose.Jwt.sign ~header ~payload jwk with
+      | Ok jwt -> jwt
+      | Error (`Msg e) -> raise_s [%message e]
+    in
+    return [%string "Bearer %{jwt#Jose.Jwt}"]
   ;;
 end
 
@@ -21,13 +44,15 @@ let post_comment repo pr contents =
   let%bind creds =
     Reader.load_sexp "creds.sexp" Github_creds.t_of_sexp >>| Or_error.ok_exn
   in
+  let%bind auth_string = Github_creds.to_auth_string creds in
+  Log.Global.info_s [%message auth_string];
   let endpoint =
     [%string
       "https://api.github.com/repos/%{repo#Repo_id}/issues/%{pr#Pull_request_id}/comments"]
   in
   let headers = Header.init () in
   let headers = Header.add headers "Accept" "application/vnd.github.v3+json" in
-  let headers = Header.add headers "Authorization" (Github_creds.to_auth_string creds) in
+  let headers = Header.add headers "Authorization" auth_string in
   let body = Body.of_string [%string "{\"body\":\"%{contents}\"}"] in
   let%bind resp, _resp_body = Client.post ~headers ~body (Uri.of_string endpoint) in
   let status = Response.status resp in
