c76aaf6df5c554f016d426648603e15bd89f08b7
Vasily Alferov <vasily.v.alferov@gmail.com>
2021-09-19 15:49:52 +0300

    z

--- lib/bot.ml
+++ lib/bot.ml
@@ -132,7 +132,16 @@ let seen_repos pg_conn =
 ;;
 
 let pack s =
-  [%string "<html><head><title>fp.vasalf.net</title></head><body>%{s}</body></html>"]
+  let bootstrap =
+    "<link \
+     href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css\" \
+     rel=\"stylesheet\" \
+     integrity=\"sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU\" \
+     crossorigin=\"anonymous\">"
+  in
+  [%string
+    "<!doctype \
+     html5><html><head><title>fp.vasalf.net</title>%{bootstrap}</head><body>%{s}</body></html>"]
 ;;
 
 let process_home pg_conn =
